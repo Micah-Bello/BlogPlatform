cfd1eea8ea01094df2114d1a89e80934b1f1d827
Micah-Bello <micahbello16@gmail.com>
2021-07-14 13:08:01 +0100

    Add project files.

---
+++ BlogPlatform/Models/Comment.cs
@@ -0,0 +1,45 @@
+﻿using BlogPlatform.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPlatform.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [Display(Name = "Comment")]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        public string Body { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        public DateTime? Moderated { get; set; }
+        public DateTime? Deleted { get; set; }
+
+        public ModerationType ModerationType { get; set; }
+
+        [Display(Name = "Moderated Comment")]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        public string ModeratedBody { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual IdentityUser Author { get; set; }
+        public virtual IdentityUser Moderator { get; set; }
+    }
+}
